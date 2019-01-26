fbf8e88698c46bc4b614bd0ba9ad5bffff082b48
Comet <kkituyi@hotmail.com>
2019-01-26 14:53:33 +0300

    EJS CHART WORKS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'neon-html';
+  public chartData: Object [];
+
+public primaryXAxis: Object;
+
+ngOnInit(): void {
+
+// Data for chart series
+
+this.chartData = [
+
+{ year:'2000', gold: 35, silver: 25 }, { year: '2001', gold: 28, silver: 20 },
+
+{ year:'2002', gold: 34, silver: 21 }, { year: '2003', gold: 32, silver: 15 },
+
+{ year:'2004', gold: 40, silver: 30 }
+
+];
+
+this.primaryXAxis = { valueType: 'Category' };
+
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,21 @@ import { FormsModule} from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { ChartComponent } from './chart/chart.component';
+import { ChartModule } from '@syncfusion/ej2-ng-charts';
+import { CategoryService, ColumnSeriesService } from '@syncfusion/ej2-ng-charts';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule, ReactiveFormsModule,
+    FormsModule, ReactiveFormsModule, ChartModule,
     NgbModule.forRoot()
   ],
-  providers: [],
+  providers: [CategoryService, ColumnSeriesService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
