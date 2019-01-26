13ced41f2f505294478ab3ad58f3cd3bba63766b
Comet <kkituyi@hotmail.com>
2019-01-26 22:55:07 +0300

    This is how we linked our ui through websockets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { WebsocketService } from './websocket.service';
+import {  RecvService } from './recv.service';
 import { Observable } from 'rxjs';
 @Component({ 
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ WebsocketService, RecvService  ]
   
 })
 export class AppComponent {
   title = 'Neon';
-  readonly progress: Observable<number>;
+  progress: Observable<number>;
 
-  constructor() {
-    this.progress = this.emulateProgress();
-  }
-
-  emulateProgress() {
-    return new Observable<number>(observer => {
-      let val = 0;
-      const interval = setInterval(() => { 
-        if (val < 100) {
-          val++;
-        } else {
-          val = 0;
-        }
-
-        observer.next(val);
-      }, 1000);
-
-      return () => {
-        clearInterval(interval);
-      };
+  setprogress(val){
+     return new Observable<number>(observer => {
+      observer.next(val);
     });
+    }
+
+  constructor(private recvService: RecvService) {
+		recvService.messages.subscribe(msg => {			
+      console.log("Response from websocket: " + msg);
+      this.progress =this.setprogress(msg.progress_val);
+		});
+   
+	}
+
+
+
+  private message = {
+		author: 'tutorialedge',
+    message: 'this is a test message',
+    progress_val:0
+	}
+
+  sendMsg() {
+		console.log('new message from client to websocket: ', this.message);
+		this.recvService.messages.next(this.message);
+		this.message.message = '';
+
   }
+
 }
---
+++ src/app/recv.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs/Rx';
+import { WebsocketService } from './websocket.service';
+
+const CHAT_URL = 'ws://localhost:9999';
+
+export interface Message {
+	author: string,
+	message: string,
+  progress_val: number
+}
+
+@Injectable()
+export class RecvService {
+
+  public messages: Subject<Message>;
+
+	constructor(wsService: WebsocketService) {
+		this.messages = <Subject<Message>>wsService
+			.connect(CHAT_URL)
+			.map((response: MessageEvent): Message => {
+				let data = JSON.parse(response.data);
+				return {
+					author: data.author,
+					message: data.message,
+          progress_val:data.progress_val
+				}
+			});
+	}
+}
