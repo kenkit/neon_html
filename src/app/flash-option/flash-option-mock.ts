831819aba9b44eaf11edc6944cbad446fd58791c
Comet <kkituyi@hotmail.com>
2019-02-08 16:08:41 +0300

    still fixing stuff

--- src/app/flash-option/flash-option-mock.ts
+++ src/app/flash-option/flash-option-mock.ts
@@ -3,7 +3,4 @@ import { flash_option  } from './flash_option';
 export const FLASHOPTIONS:flash_option[] = [
   { id: 1, name: 'Remove FRP' },
   { id: 2, name: 'Format Userdata' },
-  { id: 3, name: 'Backup Security' },
-  { id: 4, name: 'Celeritas' },
-  { id: 5, name: 'Instal ISD-2121-23RE423.ZIP' }
 ];
