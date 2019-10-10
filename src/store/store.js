ecd989660b922e911473e0c589b4e3e029c8d110
Comet <kkituyi@hotmail.com>
2019-10-10 09:37:10 +0300

    Prep neon ui updater

--- src/store/store.js
+++
@@ -1,7 +0,0 @@
-import {createStore, applyMiddleware} from 'redux';
-import reduxWebsocket from 'react-redux-websocket';
-import rootReducer from '../reducer/reducer'
-
-const socket = new WebSocket('ws://localhost:9999');
-const store = createStore(rootReducer, applyMiddleware(reduxWebsocket(socket)));
-export default store;
